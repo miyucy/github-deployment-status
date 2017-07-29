e52c9b6fffc54c2b9f50df4482c587f9e9eb4eb4
miyucy <fistfvck@gmail.com>
2017-07-30 04:18:45 +0900

    init

---
+++ lib/github/deployment/status.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal=true
+
+require 'octokit'
+require 'optparse'
+
+module Github
+  module Deployment
+    module Status
+      class CLI
+        def initialize(argv = ARGV)
+          @argv = argv
+        end
+
+        def perform
+          parse_options @argv
+          deployment = octokit.create_deployment(@repo, @ref,
+                                                 environment: @environment,
+                                                 auto_merge: false)
+          octokit.create_deployment_status(deployment[:url],
+                                           state: 'success',
+                                           target_url: @target_url,
+                                           description: @description)
+        end
+
+        def octokit
+          @octokit ||= Octokit::Client.new access_token: @access_token
+        end
+
+        def parse_options(argv)
+          OptionParser.new do |opt|
+            opt.on('-a', '--access-token', String) do |v|
+              @access_token = v
+            end
+            opt.on('-r', '--repo', String) do |v|
+              @repo = v
+            end
+            opt.on('--ref', String) do |v|
+              @ref = v
+            end
+            opt.on('--environment', String) do |v|
+              @environment = v
+            end
+            opt.on('--description', String) do |v|
+              @description = v
+            end
+            opt.on('--target-url', String) do |v|
+              @target_url = v
+            end
+
+            opt.parse argv
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/github/deployment/status/version.rb
@@ -0,0 +1,7 @@
+module Github
+  module Deployment
+    module Status
+      VERSION = "0.1.0"
+    end
+  end
+end
